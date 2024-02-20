9d1fb03a0e56f1590c27007c026c68dbe1ebd5b2
Zheng Hao Tan <zheng@modular.com>
2024-02-20 18:42:42 +0000

    add initial run.sh

---
+++ examples/inference/bert-python-torchscript/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+# Turn off entranous output messages
+export TF_CPP_MIN_LOG_LEVEL='3'
+export TRANSFORMERS_VERBOSITY='critical'
+export TOKENIZERS_PARALLELISM='false'
+
+CURRENT_DIR=$(dirname "$0")
+MODEL_PATH="bert.torchscript"
+
+# Example input for the model
+INPUT_EXAMPLE="My dog is cute."
+
+# Download model from HuggingFace
+python3 "$CURRENT_DIR/download-model.py" --text "$INPUT_EXAMPLE" -o $MODEL_PATH
+
+python3 simple-inference.py --text "$INPUT_EXAMPLE" --model-path $MODEL_PATH
