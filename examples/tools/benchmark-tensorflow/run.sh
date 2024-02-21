fb6d37e0d577bcc4ca4118c90dea157b332d2269
akirchhoff-modular <akirchhoff@modular.com>
2024-02-20 18:30:08 -0800

    Switch benchmark example to RoBERTa (#13)
    ResNet-50 currently takes longer to compile than RoBERTa does.  Swap out
    the model to RoBERTa so the user spends less time waiting for model
    compilation.  We'd switch the visualize example too, but RoBERTa is a
    more complicated model that pushes the limits of Netron -- ResNet-50 is
    more reasonably sized and works better with Netron, even though it takes
    longer to compile in MAX.
    
    Co-authored-by: Zheng Hao Tan <zheng@modular.com>

--- examples/tools/benchmark-tensorflow/run.sh
+++ examples/tools/benchmark-tensorflow/run.sh
@@ -6,10 +6,11 @@ set -e
 # Make sure we're running from inside the directory containing this file.
 cd "$(dirname "$0")"
 
-# If ResNet50 hasn't been downloaded yet, download it.
-if ! [ -d ../common/resnet50-tensorflow/resnet50-savedmodel ]; then
-	../common/resnet50-tensorflow/download-model.sh -o ../common/resnet50-tensorflow/resnet50-savedmodel
+# If RoBERTa hasn't been downloaded yet, download it.
+if ! [ -d ../common/roberta-tensorflow/roberta-savedmodel ]; then
+	../common/roberta-tensorflow/download-model.sh -o ../common/roberta-tensorflow/roberta-savedmodel
 fi
 
 # Now for the easy part -- benchmarking ;)
-max benchmark ../common/resnet50-tensorflow/resnet50-savedmodel
+# Even though this is a TensorFlow model, we need --input-data-schema in order to override how the inputs are generated, since the inputs must have a fixed range and the model has dynamic input shapes.
+max benchmark  --input-data-schema=../common/roberta-tensorflow/input-spec.yaml ../common/roberta-tensorflow/roberta-savedmodel
---
+++ examples/tools/common/roberta-pytorch/download-model.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+"""Download RoBERTa PyTorch model from HuggingFace."""
+
+import argparse
+import os
+import sys
+import tempfile
+import traceback
+import urllib.request
+
+
+INSTALL_PROSE = """Is it installed?
+
+If not, try:
+
+    python3 -m venv venv; source venv/bin/activate  # if you aren't already in a virtual environment
+    pip install torch
+
+"""
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="path to save TorchScript model to",
+        required=True,
+    )
+    args = parser.parse_args()
+
+    if os.path.exists(args.output):
+        print(f"output {args.output!r} already exists")
+        return
+
+    print("Importing PyTorch...", flush=True)
+    try:
+        import torch
+    except ModuleNotFoundError:
+        with tempfile.NamedTemporaryFile(
+            mode="w", prefix="torchimport", suffix=".log", delete=False
+        ) as log_file:
+            traceback.print_exc(file=log_file)
+        print(
+            (
+                f"Torch module was not found.  {INSTALL_PROSE}"
+                f"Detailed error info in {log_file.name}"
+            ),
+            file=sys.stderr,
+        )
+        sys.exit(1)
+    except ImportError:
+        with tempfile.NamedTemporaryFile(
+            mode="w", prefix="torchimport", suffix=".log", delete=False
+        ) as log_file:
+            traceback.print_exc(file=log_file)
+        print(
+            (
+                "PyTorch installation seems to be broken.\n"
+                "Make sure you can 'import torch' "
+                "from a Python prompt and try again.\n"
+                f"Detailed error info in {log_file.name}"
+            ),
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    print("Importing HuggingFace transformers...", flush=True)
+    try:
+        from transformers import RobertaForSequenceClassification
+    except ModuleNotFoundError:
+        with tempfile.NamedTemporaryFile(
+            mode="w", prefix="hfimport", suffix=".log", delete=False
+        ) as log_file:
+            traceback.print_exc(file=log_file)
+        print(
+            (
+                "HuggingFace transformers module was not found. "
+                f" {INSTALL_PROSE}Detailed error info in {log_file.name}"
+            ),
+            file=sys.stderr,
+        )
+        sys.exit(1)
+    except ImportError:
+        with tempfile.NamedTemporaryFile(
+            mode="w", prefix="hfimport", suffix=".log", delete=False
+        ) as log_file:
+            traceback.print_exc(file=log_file)
+        print(
+            (
+                "HuggingFace transformers installation seems to be broken.\n"
+                "Make sure you can 'import transformers' "
+                "from a Python prompt and try again.\n"
+                f"Detailed error info in {log_file.name}"
+            ),
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    print("Getting pre-trained model...", flush=True)
+    model = RobertaForSequenceClassification.from_pretrained(
+        "cardiffnlp/twitter-roberta-base-emotion-multilabel-latest"
+    )
+    print("Tracing model...", flush=True)
+    traced_model = torch.jit.trace(
+        model,
+        [
+            torch.zeros(1, 128, dtype=torch.int64),
+            torch.ones(1, 128, dtype=torch.float32),
+            torch.zeros(1, 128, dtype=torch.int64),
+        ],
+        strict=False,
+    )
+    print("Saving TorchScript...", flush=True)
+    traced_model.save(args.output)
+    print("All done!", flush=True)
+
+
+if __name__ == "__main__":
+    main()
