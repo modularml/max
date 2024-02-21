ac43db8f40f20cf0214367f2dec0f314f76bf0a9
Alexandr Nikitin <anikitin@modular.com>
2024-02-21 00:43:24 +0000

    Add MAX Serving BERT PyTorch example

---
+++ examples/inference/bert-python-torchscript/deploy.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# If anything goes wrong, stop running the script.
+set -e
+
+CURRENT_DIR=$(dirname "$0")
+MODEL_PATH="bert.torchscript"
+MODEL_DIR="bert"
+
+# Example input for the model
+INPUT_EXAMPLE="There are many exciting developments in the field of AI Infrastructure!"
+
+# Download model from HuggingFace
+python3 "$CURRENT_DIR/download-model.py" -o $MODEL_PATH
+
+echo "Preparing model repository"
+# Triton expects models to reside in the specific layout, i.e.
+# <model-repository-path>/
+#   <model-name>/
+#     [config.pbtxt]
+#     <version>/
+#       <model-definition-file>
+# see https://github.com/triton-inference-server/server/blob/main/docs/user_guide/model_repository.md
+# https://github.com/triton-inference-server/server/blob/main/docs/user_guide/model_repository.md#tensorflow-models
+
+mkdir -p model-repository/${MODEL_DIR}/1/
+cp -r ${MODEL_PATH} model-repository/${MODEL_DIR}/1/
+cp config.pbtxt model-repository/${MODEL_DIR}/
+
+echo "Starting container"
+CONTAINER_ID=$(\
+  docker run --rm -d --net=host --ipc=host \
+    -v $(pwd)/model-repository:/model-repository \
+    public.ecr.aws/o4d0h4e7/max-serving-de \
+    tritonserver --model-repository=/model-repository \
+    --load-model=${MODEL_DIR} \
+)
+
+printf "Compiling ${MODEL_DIR} model"
+until curl --output /dev/null --silent --fail localhost:8000/v2/health/ready; do
+    printf '.'
+    sleep 5
+done
+printf "\nMAX Serving container started\n\n"
+
+# Execute the model with example input
+python3 triton-inference.py --text "$INPUT_EXAMPLE"
+
+printf "\nStopping container..."
+docker stop $CONTAINER_ID > /dev/null
+printf "\nDone.\n"
---
+++ examples/inference/bert-python-torchscript/triton-inference.py
@@ -0,0 +1,111 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+
+from argparse import ArgumentParser
+import tritonclient.http as httpclient
+from transformers import AutoTokenizer
+
+
+os.environ["TRANSFORMERS_VERBOSITY"] = "critical"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+BATCH = 1
+SEQLEN = 128
+DEFAULT_MODEL_NAME = "bert"
+DESCRIPTION = "BERT model"
+HF_MODEL_NAME = "bert-base-uncased"
+
+def execute(triton_client, model_name, inputs):
+    # Set the input data
+    triton_inputs = [
+        httpclient.InferInput("input_ids", inputs["input_ids"].shape, "INT64"),
+        httpclient.InferInput(
+            "attention_mask", inputs["attention_mask"].shape, "INT64"
+        ),
+        httpclient.InferInput(
+            "token_type_ids", inputs["token_type_ids"].shape, "INT64"
+        ),
+    ]
+    triton_inputs[0].set_data_from_numpy(inputs["input_ids"].astype(np.int64))
+    triton_inputs[1].set_data_from_numpy(
+        inputs["attention_mask"].astype(np.int64)
+    )
+    triton_inputs[2].set_data_from_numpy(
+        inputs["token_type_ids"].astype(np.int64)
+    )
+
+    print("Executing model...")
+    results = triton_client.infer(model_name, triton_inputs)
+    print("Model executed.\n")
+
+    return results
+
+
+def main():
+    # Parse args
+    parser = ArgumentParser(description=DESCRIPTION)
+    parser.add_argument(
+        "--text",
+        type=str,
+        metavar="<text>",
+        required=True,
+        help="Statement to classify.",
+    )
+    parser.add_argument(
+        "--model-name",
+        type=str,
+        default=DEFAULT_MODEL_NAME,
+        help="Model name to execute inference.",
+    )
+    parser.add_argument(
+        "--url",
+        type=str,
+        required=False,
+        default="localhost:8000",
+        help="Inference server URL. Default is localhost:8000.",
+    )
+    args = parser.parse_args()
+
+    # Create a triton client
+    triton_client = httpclient.InferenceServerClient(url=args.url)
+
+    # Preprocess input statement
+    print("Processing input...")
+    tokenizer = AutoTokenizer.from_pretrained(HF_MODEL_NAME)
+    inputs = tokenizer(
+        args.text,
+        return_tensors="np",
+        return_token_type_ids=True,
+        padding="max_length",
+        truncation=True,
+        max_length=SEQLEN,
+    )
+    print("Input processed.\n")
+
+    # Classify input statement
+    outputs = execute(triton_client, args.model_name, inputs)
+
+    # Extract class prediction from output
+    predicted_class_id = outputs["result0"]["logits"].argmax(axis=-1)[0]
+    predicted_label = predicted_class_id.item()
+    sentiment_labels = {0: "Positive", 1: "Negative"}
+    print(f"Predicted sentiment: {sentiment_labels[predicted_label]}")
+
+
+if __name__ == "__main__":
+    main()
