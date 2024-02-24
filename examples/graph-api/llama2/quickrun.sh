a1426ebe5dc7c54c3f0057bcf71c6de3d4f3d472
Stef Lindall <bethebunny@gmail.com>
2024-02-23 17:32:51 -0800

    [Graph API] llama.🔥 initial commit (#30)
    Adds llama.🔥 examples for Graph API

---
+++ examples/graph-api/llama2/quickrun.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+
+##===----------------------------------------------------------------------===##
+#
+# This file is Modular Inc proprietary.
+#
+##===----------------------------------------------------------------------===##
+
+# Usage: ./quickrun.sh [model_file]
+
+SCRIPT_PATH="$(readlink -f "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+
+source "$SCRIPT_DIR/setup.sh"
+
+mojo \
+    -D LLAMA_MODEL_PATH="${1:-$MODELS/stories15M.bin}" \
+    -D TOKENIZER_PATH="$MODELS/tokenizer.bin" \
+    -I "$SCRIPT_DIR/tokenizer" \
+    "$SCRIPT_DIR/run.🔥"
