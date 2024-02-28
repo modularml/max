f2cdb1df8a4dcbfa00d32f661123c82fb8e9432f
Zheng Hao Tan <zheng@modular.com>
2024-02-28 21:20:53 +0000

    update visualize tensorflow

--- examples/tools/visualize-tensorflow/run.sh
+++ examples/tools/visualize-tensorflow/run.sh
@@ -7,12 +7,12 @@ set -e
 cd "$(dirname "$0")"
 
 # If ResNet50 hasn't been downloaded yet, download it.
-if ! [ -d ../common/resnet50-tensorflow/resnet50-savedmodel ]; then
-	../common/resnet50-tensorflow/download-model.sh -o ../common/resnet50-tensorflow/resnet50-savedmodel
+if ! [ -d ../../models/resnet50-tensorflow ]; then
+	../common/resnet50-tensorflow/download-model.sh -o ../../models/resnet50-tensorflow
 fi
 
 # Now for the easy part -- visualization ;)
-max visualize ../common/resnet50-tensorflow/resnet50-savedmodel
+max visualize ../../models/resnet50-tensorflow
 if [ "$CI" != true ]; then
 	python3 -m webbrowser https://netron.app || true
 fi
