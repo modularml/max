4e31e9fda7da4dc3360cd1d77ffe9aa33ed09b04
Zheng Hao Tan <zheng@modular.com>
2024-02-28 18:36:29 +0000

    revert deploy.sh

--- examples/inference/roberta-python-tensorflow/deploy.sh
+++ examples/inference/roberta-python-tensorflow/deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 INPUT_EXAMPLE="There are many exciting developments in the field of AI Infrastructure!"
-MODEL_DIR="../../models/roberta-tensorflow"
+MODEL_DIR="roberta"
 
 # Make sure we're running from inside the directory containing this file.
 cd "$(dirname "$0")"
