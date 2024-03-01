b8b65b3b729a0a6a2e59387ace3af0d02478ae1d
Zheng Hao Tan <zheng@modular.com>
2024-03-01 22:01:29 +0000

    refactor bert-python-torchscript into common/ subdir

--- examples/inference/bert-python-torchscript/run.sh
+++ examples/inference/bert-python-torchscript/run.sh
@@ -24,6 +24,6 @@ MODEL_PATH="../../models/bert.torchscript"
 cd "$(dirname "$0")"
 
 # Download model from HuggingFace
-python3 download-model.py -o "$MODEL_PATH"
+python3 ../common/bert-torchscript/download-model.py -o "$MODEL_PATH"
 
 python3 simple-inference.py --text "$INPUT_EXAMPLE" --model-path "$MODEL_PATH"
