c69f5d53a840827328a99303c9d66852731b6d42
Sean Paradiso <sparadiso@modular.com>
2024-05-17 16:26:43 -0400

    (Re-)Introduce a simple Mojo Engine API example using Torchscript to run masked inference on a masked BERT variant.
    MODULAR_ORIG_COMMIT_REV_ID: e4e04228e462168a7f16e94bb406d289be37c14a

---
+++ examples/inference/bert-mojo-torchscript/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##===----------------------------------------------------------------------===##
+
+set -e
+
+# Example input for the model
+INPUT_EXAMPLE="Paris is the [MASK] of France."
+
+MODEL_PATH="../../models/bert-mlm.torchscript"
+
+# Make sure we're running from inside the directory containing this file.
+cd "$(dirname "$0")"
+
+# Download model from HuggingFace
+python3 ../common/bert-torchscript/download-model.py -o "$MODEL_PATH" --mlm
+
+mojo run.🔥 "$INPUT_EXAMPLE"
