42ca4b2cfc57c504376c56522f00ddd24404156c
Jack Clayton <jack@modular.com>
2024-06-18 11:38:39 -0700

    - Adds a GUI example llama3 chat interface with streamlit. - Fix some code that didn't compile on Linux - ALlow starting the llama3 server without a prompt - Add default prompt for run-pipeline llama3, with a tip on how to provide a prompt
    MODULAR_ORIG_COMMIT_REV_ID: 85ba662fdce09667a588a85fa3649c863dc65da5

---
+++ examples/gui/README.md
@@ -0,0 +1,27 @@
+# MAX Examples GUI
+
+Example project showing how to create a GUI that calls MAX using the Python
+[streamlit](https://streamlit.io/) library. This will encompass all MAX examples
+over time.
+
+## Quick Start
+
+Install MAX if you haven't already: [modul.ar/install-max](https://modul.ar/install-max)
+
+Pixi is the fastest way to create a reproducible Python environment:
+
+```bash
+curl -fsSL https://pixi.sh/install.sh | bash
+```
+
+Restart your shell then run:
+
+```bash
+pixi run gui
+```
+
+## Working Examples
+
+### Interactive chatbot using llama3 from MAX
+
+![alt text](images/llama3.png)
---
+++ examples/gui/pages/llama3.py
@@ -0,0 +1,159 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+
+import os
+import openai
+import subprocess
+import streamlit as st
+
+from shared import menu, kill_process, download_file
+
+st.set_page_config("Llama3", page_icon="🦙")
+
+"""
+# 🦙 Llama3
+
+Select a quantization encoding to download model from a predefined `Model URL`. If the model exists at `Model Path` it won't be downloaded again. You can set a custom `Model URL` or `Model Path` that matches the quantization encoding.
+"""
+
+menu()
+
+model_state = st.empty()
+
+
+@st.cache_resource(show_spinner=False)
+def start_llama3(
+    temperature,
+    max_tokens,
+    min_p,
+    custom_ops_path,
+    tokenizer_path,
+    quantization,
+    model_path,
+):
+    with st.spinner("Starting Llama3"):
+        model_state = st.empty()
+        # Kill server if it's already running
+        kill_process(8000, model_state)
+        command = [
+            "mojo",
+            "run",
+            "../graph-api/serve_pipeline.🔥",
+            "llama3",
+            "--max-tokens",
+            str(max_tokens),
+            "--model-path",
+            model_path,
+            "--prompt",
+            "start",
+            "--quantization-encoding",
+            quantization,
+            "--temperature",
+            str(temperature),
+            "--min-p",
+            str(min_p),
+        ]
+        if custom_ops_path:
+            command.append("--custom-ops-path")
+            command.append(custom_ops_path)
+        if tokenizer_path:
+            command.append("--tokenizer-path")
+            command.append(tokenizer_path)
+
+        process = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+        )
+        if process.stderr:
+            model_state.error(process.stderr, icon="🚨")
+
+        # Generator to yield characters from the subprocess output
+        while process.poll() is None:
+            line = process.stdout.readline()
+            if line:
+                if line.strip() == "Listening on port 8000!":
+                    model_state.success("Llama3 is ready!", icon="✅")
+                    return
+                if line.strip().startswith("mojo: error:"):
+                    model_state.error(line, icon="🚨")
+                    exit(1)
+                else:
+                    model_state.info(line, icon="🛠️")
+
+
+client = openai.OpenAI(api_key="NA", base_url="http://localhost:8000/v1")
+
+quantization = st.sidebar.selectbox(
+    "Quantization Encoding", ["q4_k", "q4_0", "q6_k"]
+)
+
+if quantization == "q4_0":
+    model_url = "https://huggingface.co/QuantFactory/Meta-Llama-3-8B-GGUF/resolve/main/Meta-Llama-3-8B.Q4_0.gguf"
+elif quantization == "q4_k":
+    model_url = "https://huggingface.co/bartowski/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct-Q4_K_M.gguf"
+elif quantization == "q6_k":
+    model_url = "https://huggingface.co/bartowski/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct-Q6_K.gguf"
+
+model_url = st.sidebar.text_input("Model URL", value=model_url)
+model_path = os.path.join("models", os.path.basename(model_url))
+model_path = st.sidebar.text_input("Model Path", value=model_path)
+
+download_file(model_url, model_path, model_state)
+
+temperature = st.sidebar.slider("Temperature", 0.0, 1.0, 0.5)
+max_tokens = st.sidebar.slider("Max Tokens", 0, 1024, 512)
+min_p = st.sidebar.slider("Minimum Probability Threshold", 0.0, 1.0, 0.05)
+custom_ops_path = st.sidebar.text_input("Custom Ops Path")
+tokenizer_path = st.sidebar.text_input("Tokenizer Path")
+
+start_llama3(
+    temperature,
+    max_tokens,
+    min_p,
+    custom_ops_path,
+    tokenizer_path,
+    quantization,
+    model_path,
+)
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"], avatar=message["avatar"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Send a message to llama3"):
+    st.session_state.messages.append(
+        {"role": "user", "avatar": "💬", "content": prompt}
+    )
+
+    with st.chat_message("user", avatar="💬"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant", avatar="🦙"):
+        stream = client.chat.completions.create(
+            model="",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+                if m["role"] == "user"
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response, "avatar": "🦙"}
+    )
