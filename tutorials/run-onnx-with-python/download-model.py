406f1d741ca5fa38145d2e69449678ca7e435796
Scott Main <scott@modular.com>
2024-07-19 13:09:33 -0700

    Add finished code for **** inference tutorial
    This is the finished code that users will create themselves when they
    finish the "Run an **** model with Python" tutorial.
    
    This code exists in the MAX repo primarily for CI testing, to ensure
    that the tutorial code is functional, but also as a reference for users
    to compare their finished work (or to cheat).
    
    Notice that this creates a new top-level `tutorials` path in the MAX
    repo, alongside the `examples` path. This is important because the
    finished code for tutorials should not be the same as the other code
    examples. For example, the code for this ResNet tutorial is much more
    simple than the other "inference" code example using ResNet. This is
    crucial for simplicity when building the project in a tutorial. The code
    in each tutorial project is designed to work stand-alone and does not
    depend on other code within the repo—it is fully self-contained in its
    own folder within the `tutorials/` path.
    
    MODULAR_ORIG_COMMIT_REV_ID: a7ace6e18e22395d278e1b17aed49322d4042fac

---
+++ tutorials/run-onnx-with-python/download-model.py
@@ -0,0 +1,47 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+
+import torch
+from transformers import ResNetForImageClassification
+from torch.onnx import export
+
+# The HuggingFace model name and exported file name
+HF_MODEL_NAME = "microsoft/resnet-50"
+MODEL_PATH = "resnet50.onnx"
+
+
+def main():
+    # Load the ResNet model from HuggingFace in evaluation mode
+    model = ResNetForImageClassification.from_pretrained(HF_MODEL_NAME)
+    model.eval()
+
+    # Create random input for tracing, then export the model to ONNX
+    dummy_input = torch.randn(1, 3, 224, 224)
+    export(
+        model,
+        dummy_input,
+        MODEL_PATH,
+        opset_version=11,
+        input_names=["pixel_values"],
+        output_names=["output"],
+        dynamic_axes={
+            "pixel_values": {0: "batch_size"},
+            "output": {0: "batch_size"},
+        },
+    )
+
+    print(f"Model saved as {MODEL_PATH}")
+
+
+if __name__ == "__main__":
+    main()
