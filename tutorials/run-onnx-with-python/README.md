8eb87f78f9711dc0ffc902afc9a22c02735024b1
Scott Main <scott@modular.com>
2024-07-19 13:09:33 -0700

    Add finished code for **** inference tutorial
    This is the finished code that users will create themselves when they
    finish the "Run an **** model with Python" tutorial.
    
    This code exists in the MAX repo primarily for CI testing, to ensure
    that the tutorial code is functional, but also as a reference for users
    to compare their finished work (or to cheat).
    
    Notice that this creates a new top-level `tutorials` path in the MAX
    repo, alongside the `examples` path. This is important because the
    finished code for tutorials should not be the same as the other code
    examples. For example, the code for this ResNet tutorial is much more
    simple than the other "inference" code example using ResNet. This is
    crucial for simplicity when building the project in a tutorial. The code
    in each tutorial project is designed to work stand-alone and does not
    depend on other code within the repo—it is fully self-contained in its
    own folder within the `tutorials/` path.
    
    MODULAR_ORIG_COMMIT_REV_ID: a7ace6e18e22395d278e1b17aed49322d4042fac

---
+++ tutorials/run-onnx-with-python/README.md
@@ -0,0 +1,6 @@
+# Tutorial: Run an ONNX model with Python
+
+This is the finished project for the [Run an ONNX model with
+Python](https://docs.modular.com/max/tutorials/run-onnx-with-python) tutorial.
+
+Please refer to that page for more information.
---
+++ tutorials/run-onnx-with-python/run.py
@@ -0,0 +1,50 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+from datasets import load_dataset
+import numpy as np
+from max import engine
+
+# The HuggingFace model name and exported file name
+HF_MODEL_NAME = "microsoft/resnet-50"
+MODEL_PATH = "resnet50.onnx"
+
+
+def main():
+    dataset = load_dataset("huggingface/cats-image", trust_remote_code=True)
+    image = dataset["test"]["image"][0]
+    # optionally, save the image to see it yourself:
+    # image.save("cat.png")
+
+    image_processor = AutoImageProcessor.from_pretrained(HF_MODEL_NAME)
+    inputs = image_processor(image, return_tensors="np")
+
+    print("Keys:", inputs.keys())
+    print("Shape:", inputs["pixel_values"].shape)
+
+    session = engine.InferenceSession()
+    model = session.load(MODEL_PATH)
+    outputs = model.execute(**inputs)
+
+    print("Output shape:", outputs["output"].shape)
+
+    predicted_label = np.argmax(outputs["output"], axis=-1)[0]
+    hf_model = AutoModelForImageClassification.from_pretrained(HF_MODEL_NAME)
+    predicted_class = hf_model.config.id2label[predicted_label]
+
+    print(f"Prediction: {predicted_class}")
+
+
+if __name__ == "__main__":
+    main()
