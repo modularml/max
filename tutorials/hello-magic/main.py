ddf75e8cd289850ae852b047f06632edcedd1f61
Ehsan M. Kermani <6980212+ehsanmok@users.noreply.github.com>
2024-09-10 14:17:12 -0700

    This PR adds the accompanying code for the magic tutorial.
    MODULAR_ORIG_COMMIT_REV_ID: 59456cecc68581e373f740d80618a4673cf711b5

---
+++ tutorials/hello-magic/main.py
@@ -0,0 +1,46 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+
+import subprocess
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello, magic!"}
+
+
+@app.get("/zero")
+def zero():
+    try:
+        p = subprocess.Popen(
+            ["magic", "run", "mojo", "local/zero.mojo"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+        )
+        while True:
+            output = p.stdout.readline()
+            if output == "" and p.poll() is not None:
+                raise HTTPException(
+                    status_code=500, detail="Failed to produce zero"
+                )
+
+            return {"message": f"answer is {output}"}
+
+    except subprocess.SubprocessError as e:
+        raise HTTPException(
+            status_code=500, detail="Failed to execute subprocess"
+        )
