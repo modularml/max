3a4748063ea8d279cc8edaa29dd8771672011e6d
kcaverly <kylebcaverly@gmail.com>
2024-10-01 13:35:17 -0500

    [Pipelines] Move KVCache to kv_caching submodule and rename NaiveKVCache
    MODULAR_ORIG_COMMIT_REV_ID: 45716d2e07c1f4fd0aec5438b24bbec41ef92b0a

--- pipelines/python/nn/kv_cache.py
+++ pipelines/python/nn/kv_cache.py
@@ -34,56 +34,6 @@ from max.graph import (
 from .kv_caching import KVCacheLayout, KVCacheParams
 
 
-class KVCache:
-    keys: npt.NDArray
-    values: npt.NDArray
-    sequence_length: int
-
-    def __init__(
-        self,
-        max_length: int,
-        max_batch_size: int,
-        n_layers: int,
-        n_kv_heads: int,
-        head_dim: int,
-    ):
-        self.keys = np.zeros(
-            shape=(max_length, n_layers, max_batch_size, n_kv_heads, head_dim),
-            dtype=np.float32,
-        )
-        self.values = np.zeros(
-            shape=(max_length, n_layers, max_batch_size, n_kv_heads, head_dim),
-            dtype=np.float32,
-        )
-        self.sequence_length = 0
-
-    def update(self, new_keys: npt.NDArray, new_values: npt.NDArray):
-        """Insert the updated key and value cache elements in the main cache."""
-        key_length = new_keys.shape[0]
-        new_sequence_length = self.sequence_length + key_length
-        assert new_sequence_length <= self.keys.shape[0], (
-            f"kv-cache overflow, desired: {new_sequence_length}, "
-            f"max: {self.keys.shape[0]}"
-        )
-        batch_size = new_keys.shape[2]
-
-        self.keys[
-            self.sequence_length : new_sequence_length, :, :batch_size, ...
-        ] = new_keys
-        self.values[
-            self.sequence_length : new_sequence_length, :, :batch_size, ...
-        ] = new_values
-        self.sequence_length = new_sequence_length
-
-    def keys_view(self, batch_size: int) -> npt.NDArray:
-        """A view into the main key cache."""
-        return self.keys[0 : self.sequence_length, :, :batch_size, ...]
-
-    def values_view(self, batch_size: int) -> npt.NDArray:
-        """A view into the main value cache."""
-        return self.values[0 : self.sequence_length, :, :batch_size, ...]
-
-
 class ContiguousKVCacheType(OpaqueType):
     """Contiguous Mojo KV cache type."""
 
