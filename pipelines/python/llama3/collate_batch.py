ccbfe47fc97ddfe5999e6b8742d41416a74ec84b
Brendan Duke <bduke@modular.com>
2024-10-01 20:10:54 -0400

    [SDK] Add a collate batch function
    Add a collate batch function, which is used to pad a batch of
    `next_tokens` with either left or right padding.
    
    MODULAR_ORIG_COMMIT_REV_ID: 6c609131ec1753c25569089d0e30ba2228d204fd

---
+++ pipelines/python/llama3/collate_batch.py
@@ -0,0 +1,70 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+"""Batch padding."""
+
+from __future__ import annotations
+
+import enum
+from typing import Sequence
+
+import numpy as np
+
+
+class PaddingDirection(enum.Enum):
+    """Padding (from) direction for batch collation."""
+
+    LEFT = "left"
+    RIGHT = "right"
+
+
+def collate_batch(
+    batch: Sequence[np.ndarray],
+    direction: PaddingDirection = PaddingDirection.LEFT,
+    pad_value: int = 0,
+    batch_size: int | None = None,
+) -> np.ndarray:
+    """Generates a single batch tensor from a batch of inputs.
+
+    These input tensors may have different lengths. The `pad_value` will be used
+    to pad out the inputs to the same length.
+
+    If `batch_size` is present, add additional values to the batch up to that
+    size.
+
+    Returns:
+        A matrix with all rows padded to the max sequence length.
+
+    Raises:
+        ValueError: if the batch is empty.
+        NotImplementedError: if the batch contains anything other than vectors.
+    """
+    if not batch:
+        msg = "Must provide at least one batch item."
+        raise ValueError(msg)
+
+    if not all(a.ndim == 1 for a in batch):
+        msg = "Collate only supports rank 1 tensors for now."
+        raise NotImplementedError(msg)
+
+    pad_to = max((len(a) for a in batch), default=0)
+
+    def pad(a: np.ndarray) -> np.ndarray:
+        npad = pad_to - len(a)
+        padding = (npad, 0) if direction == PaddingDirection.LEFT else (0, npad)
+        return np.pad(a, padding, mode="constant", constant_values=pad_value)
+
+    if batch_size is not None:
+        pad_batch_item = np.array([pad_value] * pad_to)
+        batch = [*batch, *([pad_batch_item] * (batch_size - len(batch)))]
+
+    return np.stack([pad(a) for a in batch], axis=0)
