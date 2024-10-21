8e186bd5f167374ceec4db8118b2fb813a36b147
Kei-Ming Kwong <kming@users.noreply.github.com>
2024-10-21 16:44:36 -0400

    [sdk] Update use-gpu to accept device-id
    MODULAR_ORIG_COMMIT_REV_ID: 653eb669b66c3543701a0cccffcf0ce1b40a3aa4

--- pipelines/python/utils/__init__.py
+++ pipelines/python/utils/__init__.py
@@ -13,5 +13,6 @@
 """Pipeline utilities."""
 
 from .config_to_flag import config_to_flag
+from .custom_options import DevicesOptionType
 from .hf_tokenizer import tokenizer_from_gguf
 from .metrics import TextGenerationMetrics
