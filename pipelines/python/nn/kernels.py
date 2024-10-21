6650b77ee9ed95748234e0376e8076c94d5dddae
kcaverly <kylebcaverly@gmail.com>
2024-10-21 13:08:06 -0400

    [Pipelines] Add custom op wrapper for flash attention with causal mask
    MODULAR_ORIG_COMMIT_REV_ID: 36cd1341dc3205ed85b82845604820d976b929bb

--- pipelines/python/nn/kernels.py
+++ pipelines/python/nn/kernels.py
@@ -77,6 +77,41 @@ def flash_attention(
     )[0]
 
 
+def flash_attention_with_causal_mask(
+    kv_params: KVCacheParams,
+    input: TensorValue,
+    k_cache: ContinuousBatchingKVCache,
+    v_cache: ContinuousBatchingKVCache,
+    valid_lengths: TensorValue,
+) -> TensorValue:
+    """Computes flash attention provided the mo.opaque KV Cache.
+    Notably, materializes the causal mask within the kernel."""
+
+    if input.shape[0] != valid_lengths.shape[0]:
+        msg = (
+            "expected batch size of input, to equal length of valid_lengths"
+            f" got batch size of input ({input.shape[0]}), length of"
+            f" valid_lengths ({valid_lengths.shape[0]})"
+        )
+        raise ValueError(msg)
+
+    if input.dtype != kv_params.dtype:
+        msg = (
+            f"expected input to be dtype: {kv_params.dtype}, got {input.dtype}"
+        )
+        raise ValueError(msg)
+
+    op_name = f"flash_attention_kv_cache_h{kv_params.n_kv_heads}_d{kv_params.head_dim}_causal_mask_continuous_batch"
+
+    # NOTE: The scale argument to the flash attentionkernel is constrained to float32.
+    scale = ops.rsqrt(ops.constant(kv_params.head_dim, dtype=DType.float32))
+    return ops.custom(
+        op_name,
+        [input, k_cache, v_cache, valid_lengths, scale],
+        [TensorType(dtype=input.dtype, shape=input.shape)],
+    )[0]
+
+
 def key_cache_for_layer(
     kv_params: KVCacheParams,
     i: int,
