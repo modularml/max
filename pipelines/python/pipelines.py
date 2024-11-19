3e0f281327ca99e706c65a4dbf7672cc2f3d8ffc
kcaverly <kylebcaverly@gmail.com>
2024-11-19 10:40:20 -0800

    [Pipelines] Move serve and generate helper functions to utils.cli for simplicity
    MODULAR_ORIG_COMMIT_REV_ID: f3d4a0d2b6ef4d21251dac851cb6b5841b20fe2d

--- pipelines/python/pipelines.py
+++ pipelines/python/pipelines.py
@@ -11,20 +11,6 @@
 # limitations under the License.
 # ===----------------------------------------------------------------------=== #
 
-# ===----------------------------------------------------------------------=== #
-# Copyright (c) 2024, Modular Inc. All rights reserved.
-# p
-#
-# Licensed under the Apache License v2.0 with LLVM Exceptions:
-# https://llvm.org/LICENSE.txt
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ===----------------------------------------------------------------------=== #
-
 import asyncio
 import functools
 import logging
@@ -68,6 +54,11 @@ from uvicorn import Server
 from opentelemetry import trace
 
 from utils import DevicesOptionType, TextGenerationMetrics, config_to_flag
+from utils.cli import (
+    serve_pipeline,
+    generate_text_for_pipeline,
+    batch_config_from_pipeline_config,
+)
 
 logger = logging.getLogger(__name__)
 try:
@@ -590,105 +581,6 @@ def run_mistral(
             )
 
 
-def batch_config_from_pipeline_config(
-    pipeline_config: PipelineConfig, batch_timeout: float = 0.0
-) -> TokenGeneratorPipelineConfig:
-    if pipeline_config.cache_strategy == KVCacheStrategy.CONTINUOUS:
-        batch_config = TokenGeneratorPipelineConfig.continuous_heterogenous(
-            tg_batch_size=pipeline_config.max_cache_batch_size,
-            ce_batch_size=pipeline_config.max_cache_batch_size,
-            ce_batch_timeout=batch_timeout,
-            max_forward_steps=pipeline_config.max_num_steps,
-        )
-    elif pipeline_config.cache_strategy == KVCacheStrategy.NAIVE:
-        batch_config = TokenGeneratorPipelineConfig.dynamic_homogenous(
-            batch_size=pipeline_config.max_cache_batch_size,
-            batch_timeout=batch_timeout,
-            max_forward_steps=pipeline_config.max_num_steps,
-        )
-    else:
-        raise ValueError(
-            f"{pipeline_config.cache_strategy} caching strategy is not"
-            " supported by Serving."
-        )
-
-    logger.info(
-        "Server configured with %s caching with batch size %s",
-        pipeline_config.cache_strategy,
-        pipeline_config.max_cache_batch_size,
-    )
-
-    return batch_config
-
-
-async def serve_pipeline(
-    pipeline_config: PipelineConfig,
-    performance_fake: str = "none",
-    profile: bool = False,
-    batch_timeout: float = 0.0,
-    model_name: Union[str, None] = None,
-):
-    # Retrieve tokenizer and pipeline.
-    if performance_fake == "none":
-        logger.info(
-            f"Starting server using {pipeline_config.huggingface_repo_id}"
-        )
-        # Load tokenizer and pipeline from PIPELINE_REGISTRY.
-        # return_factory, returns a pipeline_factory as opposed to an intialized pipeline.
-        # this minimizes the amount of data travelling during server worker initialization.
-        # it also minimized the probability of a pickling issue with custom config.
-        tokenizer, pipeline_factory = PIPELINE_REGISTRY.retrieve(
-            pipeline_config, return_factory=True
-        )
-    else:
-        logger.info(
-            f"Starting server using performance fake {performance_fake}."
-        )
-        tokenizer = PerformanceFakingPipelineTokenizer(  # type: ignore
-            AutoTokenizer.from_pretrained(pipeline_config.huggingface_repo_id)
-        )
-        pipeline_factory = functools.partial(  # type: ignore
-            get_performance_fake,
-            performance_fake,  # type: ignore
-        )
-        pipeline_config.cache_strategy = KVCacheStrategy.CONTINUOUS
-
-    # Initialize settings, and TokenGeneratorPipelineConfig.
-    request_limit = pipeline_config.max_cache_batch_size + 128
-    settings = Settings(api_types=[APIType.OPENAI], request_limit=request_limit)
-    debug_settings = DebugSettings(profiling_enabled=profile)
-
-    # Load batch config.
-    batch_config = batch_config_from_pipeline_config(
-        pipeline_config=pipeline_config,
-        batch_timeout=batch_timeout,
-    )
-
-    # If explicit model name is not provided, set to huggingface_repo_id.
-    if model_name is None:
-        model_name = pipeline_config.huggingface_repo_id
-
-    # Intialize and serve webserver.
-    app = fastapi_app(
-        settings,
-        debug_settings,
-        {
-            model_name: BatchedTokenGeneratorState(  # type: ignore
-                TokenGeneratorPipeline(
-                    batch_config,
-                    pipeline_config.huggingface_repo_id,  # type: ignore
-                    tokenizer,
-                    True,
-                ),
-                pipeline_factory,  # type: ignore
-            )
-        },
-    )
-
-    server = Server(fastapi_config(app=app))
-    await server.serve()
-
-
 def common_server_options(func):
     @config_to_flag(PipelineConfig)
     @click.option(
@@ -758,14 +650,12 @@ def cli_serve(
 ):
     # Initialize config, and serve.
     pipeline_config = PipelineConfig(**config_kwargs)
-    asyncio.run(
-        serve_pipeline(
-            pipeline_config=pipeline_config,
-            profile=profile_serve,
-            performance_fake=performance_fake,
-            batch_timeout=batch_timeout,
-            model_name=model_name,
-        )
+    serve_pipeline(
+        pipeline_config=pipeline_config,
+        profile=profile_serve,
+        performance_fake=performance_fake,
+        batch_timeout=batch_timeout,
+        model_name=model_name,
     )
 
 
@@ -817,46 +707,17 @@ def replit(
     pipeline_config = PipelineConfig(**config_kwargs)
 
     if serve:
-        asyncio.run(
-            serve_pipeline(
-                pipeline_config=pipeline_config,
-                profile=profile_serve,
-                performance_fake=performance_fake,
-                batch_timeout=batch_timeout,
-                model_name=model_name,
-            )
+        serve_pipeline(
+            pipeline_config=pipeline_config,
+            profile=profile_serve,
+            performance_fake=performance_fake,
+            batch_timeout=batch_timeout,
+            model_name=model_name,
         )
     else:
-        # Load tokenizer & pipeline.
-        tokenizer, pipeline = PIPELINE_REGISTRY.retrieve(pipeline_config)
-
-        # Run timed run & print results.
-        with TextGenerationMetrics(print_report=True) as metrics:
-            if num_warmups > 0:
-                logger.info("Running warmup...")
-                for _ in range(num_warmups):
-                    asyncio.run(
-                        stream_text_to_console(
-                            pipeline,
-                            tokenizer,
-                            prompt,
-                            metrics=None,
-                            print_tokens=False,
-                            max_batch_size=pipeline_config.max_cache_batch_size,
-                        )
-                    )
-
-            logger.info("Beginning text generation...")
-            asyncio.run(
-                stream_text_to_console(
-                    pipeline,
-                    tokenizer,
-                    prompt,
-                    metrics=metrics,
-                    print_tokens=True,
-                    max_batch_size=pipeline_config.max_cache_batch_size,
-                )
-            )
+        generate_text_for_pipeline(
+            pipeline_config, prompt=prompt, num_warmups=num_warmups
+        )
 
 
 @main.command(name="generate")
@@ -896,35 +757,9 @@ def run_pipeline(use_gpu, prompt, num_warmups, **config_kwargs):
 
     # Load tokenizer & pipeline.
     pipeline_config = PipelineConfig(**config_kwargs)
-    tokenizer, pipeline = PIPELINE_REGISTRY.retrieve(pipeline_config)
-
-    # Run timed run & print results.
-    with TextGenerationMetrics(print_report=True) as metrics:
-        if num_warmups > 0:
-            logger.info("Running warmup...")
-            for _ in range(num_warmups):
-                asyncio.run(
-                    stream_text_to_console(
-                        pipeline,
-                        tokenizer,
-                        prompt,
-                        metrics=None,
-                        print_tokens=False,
-                        max_batch_size=pipeline_config.max_cache_batch_size,
-                    )
-                )
-
-        logger.info("Beginning text generation...")
-        asyncio.run(
-            stream_text_to_console(
-                pipeline,
-                tokenizer,
-                prompt,
-                metrics=metrics,
-                print_tokens=True,
-                max_batch_size=pipeline_config.max_cache_batch_size,
-            )
-        )
+    generate_text_for_pipeline(
+        pipeline_config, prompt=prompt, num_warmups=num_warmups
+    )
 
 
 if __name__ == "__main__":
---
+++ pipelines/python/utils/cli/serve.py
@@ -0,0 +1,136 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+"""Utilities for serving cli."""
+
+import asyncio
+import functools
+import logging
+from typing import Union
+from max.pipelines import PipelineConfig, PIPELINE_REGISTRY
+from max.pipelines.kv_cache import KVCacheStrategy
+from max.serve.config import APIType, Settings
+from max.serve.debug import DebugSettings
+from max.serve.api_server import fastapi_app, fastapi_config
+from max.serve.pipelines.deps import BatchedTokenGeneratorState
+from max.serve.pipelines.llm import (
+    TokenGeneratorPipeline,
+    TokenGeneratorPipelineConfig,
+)
+from max.serve.pipelines.performance_fake import (
+    PerformanceFakingPipelineTokenizer,
+    get_performance_fake,
+)
+from transformers import AutoTokenizer
+from uvicorn import Server
+
+
+logger = logging.getLogger(__name__)
+
+
+def batch_config_from_pipeline_config(
+    pipeline_config: PipelineConfig, batch_timeout: float = 0.0
+) -> TokenGeneratorPipelineConfig:
+    if pipeline_config.cache_strategy == KVCacheStrategy.CONTINUOUS:
+        batch_config = TokenGeneratorPipelineConfig.continuous_heterogenous(
+            tg_batch_size=pipeline_config.max_cache_batch_size,
+            ce_batch_size=pipeline_config.max_cache_batch_size,
+            ce_batch_timeout=batch_timeout,
+            max_forward_steps=pipeline_config.max_num_steps,
+        )
+    elif pipeline_config.cache_strategy == KVCacheStrategy.NAIVE:
+        batch_config = TokenGeneratorPipelineConfig.dynamic_homogenous(
+            batch_size=pipeline_config.max_cache_batch_size,
+            batch_timeout=batch_timeout,
+            max_forward_steps=pipeline_config.max_num_steps,
+        )
+    else:
+        raise ValueError(
+            f"{pipeline_config.cache_strategy} caching strategy is not"
+            " supported by Serving."
+        )
+
+    logger.info(
+        "Server configured with %s caching with batch size %s",
+        pipeline_config.cache_strategy,
+        pipeline_config.max_cache_batch_size,
+    )
+
+    return batch_config
+
+
+def serve_pipeline(
+    pipeline_config: PipelineConfig,
+    performance_fake: str = "none",
+    profile: bool = False,
+    batch_timeout: float = 0.0,
+    model_name: Union[str, None] = None,
+):
+    # Retrieve tokenizer and pipeline.
+    if performance_fake == "none":
+        logger.info(
+            f"Starting server using {pipeline_config.huggingface_repo_id}"
+        )
+        # Load tokenizer and pipeline from PIPELINE_REGISTRY.
+        # return_factory, returns a pipeline_factory as opposed to an intialized pipeline.
+        # this minimizes the amount of data travelling during server worker initialization.
+        # it also minimized the probability of a pickling issue with custom config.
+        tokenizer, pipeline_factory = PIPELINE_REGISTRY.retrieve(
+            pipeline_config, return_factory=True
+        )
+    else:
+        logger.info(
+            f"Starting server using performance fake {performance_fake}."
+        )
+        tokenizer = PerformanceFakingPipelineTokenizer(
+            AutoTokenizer.from_pretrained(pipeline_config.huggingface_repo_id)
+        )
+        pipeline_factory = functools.partial(
+            get_performance_fake,
+            performance_fake,
+        )
+        pipeline_config.cache_strategy = KVCacheStrategy.CONTINUOUS
+
+    # Initialize settings, and TokenGeneratorPipelineConfig.
+    request_limit = pipeline_config.max_cache_batch_size + 128
+    settings = Settings(api_types=[APIType.OPENAI], request_limit=request_limit)
+    debug_settings = DebugSettings(profiling_enabled=profile)
+
+    # Load batch config.
+    batch_config = batch_config_from_pipeline_config(
+        pipeline_config=pipeline_config,
+        batch_timeout=batch_timeout,
+    )
+
+    # If explicit model name is not provided, set to huggingface_repo_id.
+    if model_name is None:
+        model_name = pipeline_config.huggingface_repo_id
+
+    # Intialize and serve webserver.
+    app = fastapi_app(
+        settings,
+        debug_settings,
+        {
+            model_name: BatchedTokenGeneratorState(
+                TokenGeneratorPipeline(
+                    batch_config,
+                    pipeline_config.huggingface_repo_id,
+                    tokenizer,
+                    True,
+                ),
+                pipeline_factory,
+            )
+        },
+    )
+
+    server = Server(fastapi_config(app=app))
+    asyncio.run(server.serve())
