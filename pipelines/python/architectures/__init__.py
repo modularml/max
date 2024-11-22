7dea21439e3b1bc448344b244d08192a126e47eb
kcaverly <kylebcaverly@gmail.com>
2024-11-21 17:36:59 -0800

    [Pipelines] Introduce architectures module
    MODULAR_ORIG_COMMIT_REV_ID: 4f0019e62b0d2e95a9717af987cc59fcd3dbbe28

---
+++ pipelines/python/architectures/__init__.py
@@ -0,0 +1,17 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+
+from llama3 import Llama3
+from replit import ReplitModel
+
+__all__ = ["Llama3", "ReplitModel"]
