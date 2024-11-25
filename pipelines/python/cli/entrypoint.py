bca0a14bdff9b75db25195c75961f0c1fc9e102c
kcaverly <kylebcaverly@gmail.com>
2024-11-25 08:51:06 -0800

    [Pipelines] Rename utils module to cli
    MODULAR_ORIG_COMMIT_REV_ID: d68d2cff157399e561767a8ac23d8018b7ee8eab

--- pipelines/python/cli/entrypoint.py
+++ pipelines/python/cli/entrypoint.py
@@ -10,4 +10,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===----------------------------------------------------------------------=== #
-"""Pipeline utilities."""
+"""Entrypoint for generalized click cli."""
+
+import click
+
+
+class ModelGroup(click.Group):
+    def get_command(self, ctx, cmd_name):
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+        supported = ", ".join(self.list_commands(ctx))
+        ctx.fail(
+            f"Command not supported: {cmd_name}\nSupported commands:"
+            f" {supported}"
+        )
+
+
+@click.command(cls=ModelGroup)
+def run_cli():
+    pass
