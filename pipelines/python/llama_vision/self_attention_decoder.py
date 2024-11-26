ed5dfc3c7d6b4d6865133f83e41743854c0dab88
Zheng Hao Tan <zheng@modular.com>
2024-11-26 10:58:54 -0800

    [Pipelines] Integrate Self Attention layer in text model for Llama 3.2 vision. Integrate Self Attention layer in text model for Llama 3.2 vision.
    MODULAR_ORIG_COMMIT_REV_ID: f20a35eb8db5156d3eb4a8a3a39ee69f4b2f3c6d

---
+++ pipelines/python/llama_vision/self_attention_decoder.py
@@ -0,0 +1,43 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+
+"""Llama 3.2 Transformer Vision Self Attention layer."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from max.graph import TensorValue, TensorValueLike, ops
+from max.pipelines.kv_cache import (
+    ContinuousBatchingKVCacheCollection,
+    ContinuousBatchingKVCacheCollectionType,
+)
+from nn import AttentionWithRopeQKV
+
+
+@dataclass
+class SelfSdpaAttention(AttentionWithRopeQKV):
+    def __call__(
+        self,
+        x: TensorValueLike,
+        kv_collection: ContinuousBatchingKVCacheCollectionType,
+        **kwargs,
+    ) -> tuple[TensorValue, ContinuousBatchingKVCacheCollection]:
+        # This layer requires inputs to have a rank of 2, so we reshape and then
+        # reuse Attention with Rope for it.
+        batch_size, seq_len, hidden_dim = x.shape
+        x = x.reshape((batch_size * seq_len, hidden_dim))
+
+        # TODO: We need to make sure the shape of the output is the same as the
+        #       input given the additional reshape above.
+        return super().__call__(x, kv_collection, **kwargs)
