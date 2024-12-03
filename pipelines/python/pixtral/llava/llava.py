f3411b40b75b00ee360f814aa4406795a5cdef88
Aisha Mohamed <42137324+aishahasmoh@users.noreply.github.com>
2024-12-03 10:27:22 -0600

    [pipelines] Added test_llava_decoder
    MODULAR_ORIG_COMMIT_REV_ID: e29e73cf55d74424b66427fa0ac5210185a529ae

--- pipelines/python/pixtral/llava/llava.py
+++ pipelines/python/pixtral/llava/llava.py
@@ -42,7 +42,7 @@ class LlavaConditionalGeneration(Layer):
     def __call__(
         self,
         input_ids: TensorValue,  # Shape (batch_size, sequence_length). Indices of input sequence tokens in the vocabulary. Indices can be obtained from language model tokenizer.
-        pixel_values: TensorValue,  # Shape list of length n_images of (height, width, num_channels).
+        pixel_values: TensorValue,  # (height, width, num_channels).
         kv_cache_inputs: tuple[
             TensorValue, TensorValue, TensorValue, TensorValue
         ],
@@ -50,33 +50,32 @@ class LlavaConditionalGeneration(Layer):
     ) -> TensorValue:
         """
         Args:
-            input_ids (`TensorValue` of shape `(batch_size, sequence_length)`):
+            input_ids (ragged `TensorValue` of shape `(batch_size * sequence_length for each batch)`):
                 Indices of input sequence tokens in the vocabulary.
-                Image tokens are assumed to have index self.image_token_index.
+                input_ids[i] is a sequence of token ids (indices) in sequence i. Expanding inputs for
+                image tokens in LLaVa should be done in processing. Each image is represented in the
+                input_ids sequence by a sequence of patches that have index(id) = self.image_token_index.
                 The maximum number of image tokens in one sequence (prompt) =
-                    (input_ids == self.config.image_token_index).sum(1).max())
+                    (input_ids == self.image_token_index).sum(1).max())
                 Padding will be ignored by default should you provide it.
-                input_ids maps each input to a sequence of tokens representing it.
-                It maps image tokens to a sequence of patches of length n_patches
-                that all have value = self.image_token_index
-            pixel_values (`TensorValue` of shape `(batch_size, image_size, image_size, num_channels)):
+            pixel_values (`TensorValue` of shape `(batch_size, image_height, image_width, num_channels)):
                 The tensors corresponding to the input images. Pixel values can be obtained using ImageProcessor
         """
-
-        # TODO: take this as input rather than calculating it here?
         # TODO: if the input is a list, change vision_encoder input to pixel_values
-        # Obtains image last.  Output shape = (num_images=batch_size, num_patches_in_image, vision_encoder_hidden_dim)
-        # Apply multimodal projection to  hidden states from the vision encoder.
+        # Obtains image embeddings from the vision encoder.  Output shape = (num_images=batch_size, num_patches_in_image, vision_encoder_hidden_dim)
+        # TODO: Works now for batch_size=1, Maybe convert to a ragged tensor to be compatible with input embeds?
+        # Apply multimodal projection to  hidden states from the vision encoder. Output shape = (num_images, num_patches_in_image, language_model_hidden_dim)
+
         image_embeds = self.multi_modal_projector(
             self.vision_encoder(
                 [
                     pixel_values,
                 ]
             )
-        )  # image_embeds shape = (num_images, num_patches_in_image, language_model_hidden_dim)
-        # inputs_embeds shape
+        )
+        # inputs_embeds shape (total_sequence_length=text_and_image_tokens_length for all seqs,
+        #   language_model_hidden_dim)
         inputs_embeds = self.language_model.embedding(input_ids)
-        print("inputs_embeds shape = ", inputs_embeds.shape)
         # Replace image place-holders in inputs_embeds with image embeddings.
         special_image_mask = ops.broadcast_to(
             ops.unsqueeze((input_ids == self.image_token_index), -1),
@@ -86,14 +85,9 @@ class LlavaConditionalGeneration(Layer):
         inputs_embeds = ops.masked_scatter(
             inputs_embeds, special_image_mask, image_embeds
         )
-        print("inputs_embeds shape after scatter = ", inputs_embeds.shape)
-
         logits = self.language_model(inputs_embeds, kv_cache_inputs, **kwargs)
         return logits
-        # Expanding inputs for image tokens in LLaVa should be done in processing
-        # So, the input to Llava should be:
-        # input_ids which have an index (placeholder) for each token (patch) in
-        # each image rather than one token for the image.
+
         # max_tokens_per_image = (
         #     (input_ids == self.image_token_index).sum(1).max()
         # )
@@ -102,11 +96,8 @@ class LlavaConditionalGeneration(Layer):
         # n_image_embeds = n_images * n_patches_per_image
         # image_embeds.shape = [Dim(1), Dim(475), Dim(5120)]
         # n_image_embeds = image_embeds.shape[0] * image_embeds.shape[1]
-        # print("image_embeds.shape", "n_image_features", image_embeds.shape, n_image_embeds)
 
         # n_image_tokens = ops.sum(ops.sum(ops.cast((input_ids == self.image_token_index), DType.int32)))[0]
-
-        # print("n_image_tokens", n_image_tokens)
 
         # if n_image_tokens != ops.constant(int(n_image_embeds), DType.int32):
         #     raise ValueError(
