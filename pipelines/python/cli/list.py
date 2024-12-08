3035b0d87662f6f24f8e0c9b385f522380fc7daa
Dan Moldovan <26628547+mdanatg@users.noreply.github.com>
2024-12-08 13:01:10 -0500

    Minor formatting tweaks for consistency.
    MODULAR_ORIG_COMMIT_REV_ID: de2482e4a9a1bb996fb83c176ed379525b538b7e

--- pipelines/python/cli/list.py
+++ pipelines/python/cli/list.py
@@ -18,20 +18,26 @@ from max.pipelines import PIPELINE_REGISTRY, HuggingFaceFile
 def list_pipelines_to_console():
     print()
     for _, arch in PIPELINE_REGISTRY.architectures.items():
-        print(arch.name)
+        print(f"    {arch.name}")
         for version_name, version in arch.versions.items():
-            print(f"    Version: {version_name}")
+            print(f"        Version: {version_name}")
             for encoding_name, enc_and_strat in version.encodings.items():
                 encoding, _ = enc_and_strat
                 if isinstance(encoding, HuggingFaceFile):
-                    print(f"\t{encoding_name}: {encoding.repo_id}")
+                    print(f"            {encoding_name}: {encoding.repo_id}")
                 elif isinstance(encoding, list):
-                    first_encoding = encoding[0]
+                    first_encoding = encoding
                     if isinstance(first_encoding, HuggingFaceFile):
-                        print(f"\t{encoding_name}: {first_encoding.repo_id}")
+                        print(
+                            f"            {encoding_name}: {first_encoding.repo_id}"
+                        )
+                    elif isinstance(first_encoding, list):
+                        print(
+                            f"            {encoding_name}: {first_encoding[0].repo_id}"
+                        )
                     else:
-                        print(f"\t{encoding_name}: {first_encoding}")
+                        print(f"            {encoding_name}: {first_encoding}")
                 else:
-                    print(f"\t{encoding_name}: {first_encoding}")
+                    print(f"            {encoding_name}: {first_encoding}")
         print()
     print()
